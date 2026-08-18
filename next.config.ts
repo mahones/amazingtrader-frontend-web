04ae6b58c21cdfac05fb89ae28c416db85566073
mahones <sittisam2@gmail.com>
2026-08-18 18:15:35 +0000

    fix: update allowedDevOrigins to correct IP address for development

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.10.35"],
+  allowedDevOrigins: ["192.168.0.153"],
 };
 
 export default nextConfig;
